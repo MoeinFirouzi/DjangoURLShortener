9585fbb3afad6fb8fa4fd8bcac43e0146c6df397
MoeinFirouzi <firouzi.m.moein@gmail.com>
2023-06-17 15:20:26 +0330

    Add times_followed counter

--- url_shortener/api/v1/views.py
+++ url_shortener/api/v1/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from url_shortener.api.v1.serializers import URLSerializer
 from django.shortcuts import get_list_or_404
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 from url_shortener.models import Url
 
@@ -22,6 +23,7 @@ class RetrieveURLAPIView(RetrieveAPIView):
     API view to retrieve a URL object based on the provided
     short URL slug.
     """
+
     serializer_class = URLSerializer
 
     def get_object(self):
@@ -34,3 +36,14 @@ class RetrieveURLAPIView(RetrieveAPIView):
             return obj[-1]
         except Exception:
             raise NotFound({"error": "URL not found."})
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Increases the times_followed counter on each retrieve.
+        """
+        instance = self.get_object()
+        instance.times_followed = instance.times_followed + 1
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
